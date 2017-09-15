67de7f84e2f20e1563cd8eb08ea6c9e8b9c494da
Kenneth Wilhelmsen <kenneth@wilhelmsen.nu>
2017-09-15 12:15:28 +0200

    Added threaded network functionality and continuous print of output.

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
+
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <netinet/in.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <getopt.h>
+
+#define BUFF_SIZE 1024
+
+static unsigned short port = 8080;
+pthread_cond_t console_cv;
+pthread_mutex_t console_cv_lock;
 
 #pragma pack(1)
 typedef struct {
@@ -24,6 +40,7 @@ typedef struct {
     uint16_t frame_footer;
 } pk1000_t, *ppk1000_t;
 
+
 typedef struct {
     uint16_t frame_header;
 
@@ -38,10 +55,61 @@ void usage()
     exit(1);
 }
 
-int main()
+void *receiver(void *sfd)
 {
-    usage();
+    char buffer[BUFF_SIZE] = {0};
+    int sockfd = *(int*)sfd;
+    ssize_t readlen;
 
+    while(1) {
+        memset(buffer, 0, sizeof buffer);
+        readlen = read(sockfd, buffer, sizeof buffer);
+        if(readlen < 1)
+            continue;
+
+        pthread_mutex_lock(&console_cv_lock);
+        printf("Received length: %i, hex: %s, status: %s\n", readlen, buffer, readlen == 52 ? "OK" : "BAD");
+        pthread_cond_signal(&console_cv);
+        pthread_mutex_unlock(&console_cv_lock);
+    }
+}
+
+void console(int sockfd)
+{
+    char buffer[BUFF_SIZE];
+    memset(buffer, 0, sizeof buffer);
+
+    while(1)     {
+        fgets(buffer, sizeof buffer, stdin);
+        buffer[strlen(buffer)-1] = '\0';
+
+        if(strcmp(buffer, "") == 0)
+            continue;
+        if(strncmp(buffer, "exit", 4) == 0) {
+            pthread_mutex_destroy(&console_cv_lock);
+            pthread_cond_destroy(&console_cv);
+            _exit(EXIT_SUCCESS);
+        }
+    }
+}
+
+void connect_pk1000() {
+    int sockfd;
+    struct sockaddr_in serv_addr;
+
+    pthread_t receiver_thread;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = inet_addr("192.168.0.19");
+
+    connect(sockfd, (struct sockaddr*)&serv_addr, sizeof serv_addr);
+    pthread_create(&receiver_thread, NULL, receiver, (void*)&sockfd);
+    console(sockfd);
+}
+
+void test_casting() {
     char pBuffer[] = { 0x37, 0x38, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x19, 0x01,
                        0x01, 0x27, 0x02, 0x00, 0x39, 0x03, 0x00, 0x8B, 0x00, 0x00, 0x00, 0x00, 0x00,
                        0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00,
@@ -62,6 +130,10 @@ int main()
 
     printf("\n## Positions of anchors ##\n");
     for(i=0; i<4; printf("tag id = %i, x = %i, y = %i, z = %i\n", pk1000->anchors[i].id, pk1000->anchors[i].x, pk1000->anchors[i].y, pk1000->anchors[i++].z));
+}
 
+int main(int argc, char **argv)
+{
+    connect_pk1000();
     return 0;
 }
